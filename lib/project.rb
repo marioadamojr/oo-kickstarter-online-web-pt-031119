86d618c61313135e1c615931f81b3348fc886201
Mario Adamo Jr <marioadamojr@gmail.com>
2019-04-13 19:26:41 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -8,7 +8,7 @@ class Project
 
   def add_backer(backer)
     @backers << backer
-    if backer.backed_projects.exclude?(self)
+    if backer.backed_projects.!include?(self)
       backer.back_project(self)
     end
   end
